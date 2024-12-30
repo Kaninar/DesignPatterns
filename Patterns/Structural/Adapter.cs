cf06182452efb608d5679f2c621b646b26063772
Николай Ермаков <nikolaiwitch2018@gmail.com>
2024-12-30 22:57:00 +0500

    Adapter added

--- Patterns/Structural/Adapter.cs
+++ Patterns/Structural/Adapter.cs
@@ -1,6 +1,54 @@
 ﻿namespace DesignPatterns.Patterns.Structural.Adapter;
 
-public class Adapter
-{
 
+class Celsius
+{
+    public float value;
+
+    public Celsius(float t)
+    {
+        this.value = t;
+    }
+}
+
+class Farengate
+{
+    public float value;
+
+    public Farengate(float t)
+    {
+        this.value = t;
+    }
+}
+
+class WeatherReport
+{
+    public void PrintTempriture(Celsius celsius) => Console.WriteLine($"Today tempriture : {celsius.value}");
+}
+
+class CelsiousFarengateAdapter
+{
+    public Celsius CelsiusToFarengate(Farengate farengate)
+    {
+        return new Celsius( (float)Math.Round( ((farengate.value - 32) * 5) / 9, 0) );
+    }
+}
+
+class Client
+{
+    public void Main()
+    {
+        var celsius = new Celsius(33);
+        var farengate = new Farengate(33);
+        var adapter = new CelsiousFarengateAdapter();
+
+        var weather = new WeatherReport();
+        Console.WriteLine("No adapter used");
+        weather.PrintTempriture(celsius);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Adapting");
+        weather.PrintTempriture(adapter.CelsiusToFarengate(farengate));
+    }
 }
--- Program.cs
+++ Program.cs
@@ -14,5 +14,5 @@ using Facade = DesignPatterns.Patterns.Structural.Facade;
 using Flyweight = DesignPatterns.Patterns.Structural.Flyweight;
 using Proxy = DesignPatterns.Patterns.Structural.Proxy;
 
-new Builder.Client().Main();
+new Adapter.Client().Main();
 
