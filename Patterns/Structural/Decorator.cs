72e0db32214b9b84f461e6960a45e6e00b58e39e
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-01-03 21:36:58 +0500

    Decorator added

--- Patterns/Structural/Decorator.cs
+++ Patterns/Structural/Decorator.cs
@@ -1,6 +1,71 @@
 ﻿namespace DesignPatterns.Patterns.Structural.Decorator;
 
-public class Decorator
+interface IFile
 {
+    void Write(string data);
+    string Read();
+}
 
+class File : IFile
+{
+    public void Write(string data)
+    {
+        Console.WriteLine($"Запись {data}");
+    }
+
+    public string Read()
+    {
+        return "Чтение из файла";
+    }
+}
+
+abstract class FileEditor(IFile file) : IFile
+{
+    protected IFile _file = file;
+
+    public void Write(string data)
+    {
+        Console.WriteLine($"Запись {data} в файл");
+    }
+
+    public string Read()
+    {
+        return "Обычное чтение из файла";
+    }
+}
+
+class JSON(IFile file) : FileEditor(file)
+{
+    public void Write(string data)
+    {
+        Console.WriteLine($"Запись {data} в JSON");
+    }
+
+    public string Read()
+    {
+        return "Чтение из JSON";
+    }
+}
+
+class PDF(IFile file) : FileEditor(file)
+{
+    public void Write(string data)
+    {
+        Console.WriteLine($"Запись {data} в PDF");
+    }
+
+    public string Read()
+    {
+        return "Чтение из PDF";
+    }
+}
+
+public class Client
+{
+    public void Main()
+    {
+        File file = new File();
+        PDF pdf = new PDF(file);
+        Console.WriteLine("RESULT: " + pdf.Read());
+    }
 }
