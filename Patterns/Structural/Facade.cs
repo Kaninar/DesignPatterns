f8808144aafe5ef495a42044dcecfc7f9d7a3fde
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-01-03 21:37:18 +0500

    Facade added

--- Patterns/Structural/Facade.cs
+++ Patterns/Structural/Facade.cs
@@ -2,5 +2,17 @@
 
 public class Facade
 {
+    /*
+     actually very simple pattern
+    i am to lazy to do it right now
+    may be later
+     */
+}
 
+class Client
+{
+    public void Main()
+    {
+        Console.WriteLine("Check source mudila");
+    }
 }
